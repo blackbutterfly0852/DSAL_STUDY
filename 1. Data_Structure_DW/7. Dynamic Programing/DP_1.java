7721c382298bfbe5fa16f4732921c917552e1dcc
Blackbutterfly0852 <blackbutterfly0852@gmail.com>
2020-09-02 21:50:33 +0900

    200902 DP_1

--- 1. Data_Structure_DW/7. Dynamic Programing/DP_1.java
+++ 1. Data_Structure_DW/7. Dynamic Programing/DP_1.java
@@ -1,50 +1,82 @@
+// 1. N으로 표현
+// 1) 동적계획법 문제다
+// 2) 세 자리를 이용하는 경우, (한 자리와 두 자리의 조합, 두 자리와 한 자리의 조합) 동시에 진행을 해야한다.
+//    (1) "-", "/" 는 앞뒤가 변경된 경우 값이 달라질 수 있기 때문
+// 3) 즉, 각 사용한 갯수의 집합은 고유해야한다. 집합끼리 공통 분모가 있어서는 안된다.
+// 4) 기타
+//    (1) 하나의 set을 선언하고, 2개의 ArrayList에 동시에 넣을 경우 set에 데이터 추가시 ArrayList에 동시에 업데이트 된다.(같은객체)
 import java.util.*;
-// https://eoghks0521.tistory.com/entry/N%EC%9C%BC%EB%A1%9C-%ED%91%9C%ED%98%84%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4 참조
 public class DP_1 {
-    static int answer = -1;
+    static ArrayList<HashSet<Integer>> list = new ArrayList<>();
+    static HashSet<Integer> check = new HashSet<Integer>();
 
     public static int solution(int N, int number) {
-        dfs(N, number, 0, 0);
+        int answer = -1;
+        int setting = 0;
+        for (int n = 0; n < 8; n++) {
+            HashSet<Integer> set = new HashSet<Integer>();
+            set.add(10 * setting + N);
+            check.add(10 * setting + N);
+            list.add(set);
+            setting = 10 * setting + N;
+            
+        }
+
+        for (int i = 0; i < list.size(); i++) { // i = 0~7
+            for (int j = 0; j < i; j++) {
+                int m = i - j;
+                cal(i, j, m - 1);
+            }
+            if (list.get(i).contains(number)) {
+                return i + 1;
+            }
+        }
         return answer;
     }
 
-    public static void dfs(int N, int number, int count, int result) {
-        int NN = N; // 5
+    public static void cal(int i, int j, int m) {
 
-        if (count > 8) {
-            answer = -1;
-            return;
-        }
+        for (int one : list.get(j)) {
+            for (int two : list.get(m)) {
+                int add = one + two;
 
-        if (result == number) {
-            if (count < answer || answer == -1) {
-                answer = count;
+                if (!check.contains(add)) {
+                    check.add(add);
+                    list.get(i).add(add);
+                }
+                int sub = one - two;
+                if (!check.contains(sub)) {
+                    list.get(i).add(sub);
+                    check.add(sub);
+                }
+             
+                int mul = one * two;
+
+                if (!check.contains(mul)) {
+                    list.get(i).add(mul);
+                    check.add(mul);
+                }
+
+                if (two != 0) {
+                    int div = one / two;
+                    if (!check.contains(div)) {
+                        list.get(i).add(div);
+                        check.add(div);
+                    }
+                }
+
+               
             }
-            return;
-        }
-        for (int i = 1; i < 9 - count; i++) { // 1 ~ 8
-            dfs(N, number, count + i, result + NN); // (5, 12, 0+1, 5)   -> (5,12,2,5+5) ->  (5,12,2,5-5) -> (5,12,2,5*5) -> (5,12,2,5/5)
-            dfs(N, number, count + i, result - NN); // (5, 12, 0+1, -5)
-            dfs(N, number, count + i, result * NN); // (5, 12, 0+1, 0)
-            dfs(N, number, count + i, result / NN); //  X
-            NN = NN * 10 + 5;
 
         }
-
-    }
-
-    public static int addString(String n, int num) {
-        String number = "";
-        for (int i = 0; i < num; i++) {
-            number += n;
-        }
-        return Integer.parseInt(number);
     }
 
     public static void main(String[] args) {
-        int N = 5;
-        int number = 12;
+        int N = 4;
+        int number = 17;
         System.out.println(solution(N, number));
-    }
 
+      
+
+    }
 }
--- 1. Data_Structure_DW/7. Dynamic Programing/DP_1_1.java
+++ 1. Data_Structure_DW/7. Dynamic Programing/DP_1_1.java
@@ -1,64 +1,76 @@
-import java.util.*;
+
+// 다른사람 풀이
+// 나의 개념을 깔끔하게 정리한 것
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class DP_1_1 {
-    static ArrayList<HashSet<Integer>> list = new ArrayList<>();
-    public static int solution(int N, int number) {
-        HashSet<Integer> init = new HashSet<Integer>();
-        init.add(0);
-        list.add(init);
-        int answer = -1;
+    static HashSet<Integer> check = new HashSet<Integer>();
+    static ArrayList<Integer>[] cache = new ArrayList[9];
 
-        int setting = 0;
-        for (int n = 0; n < 8; n++) {
-            HashSet<Integer> set = new HashSet<Integer>();
-            set.add(10 * setting + N);
-            setting = 10 * setting + N;
-            list.add(set);
-        }
-        for (int i = 1; i < list.size(); i++) {
-            for (int j = 1; j < i; j++) {
-                int m = i - j;
-                //System.out.print(i + "는 " + j + " " + m + " ");
-                cal(i, j, m);
-
-            }
-
-            Iterator it = list.get(i).iterator();
-            while (it.hasNext()){
-                System.out.println(it.next() + " ");
-
-            }
-            System.out.println(" ");
-                            
-            if (list.get(i).contains(number)) {
-                return i;
-            }
-        }
-        return answer;
+    public static int add(int left, int right) {
+        return left + right;
     }
 
-    public static void cal(int i, int j, int m) {
+    public static int sub(int left, int right) {
+        return left - right;
+    }
 
-        Iterator it1 = list.get(j).iterator();
-        Iterator it2 = list.get(m).iterator();
+    public static int mul(int left, int right) {
+        return left * right;
+    }
 
-        while (it1.hasNext()) {
-            int one = (int) it1.next();
-            while (it2.hasNext()) {
-                int two = (int) it2.next();
-                list.get(i).add(one + two);
-                list.get(i).add(one - two);
-                list.get(i).add(two - one);
-                list.get(i).add(one * two);
-                if (two != 0) {
-                    list.get(i).add(one / two);
-                }
-                if (one != 0) {
-                    list.get(i).add(two / one);
+    public static int div(int left, int right) {
+        if (right == 0)
+            return 0;
+        return left / right;
+    }
+
+    public static void addCache(int digit, int ret) {
+        if (!check.contains(ret)) {
+            check.add(ret);
+            cache[digit].add(ret);
+        }
+    }
+
+    public static void cal(int digit, int left, int right) {
+        addCache(digit, add(left, right));
+        addCache(digit, sub(left, right));
+        addCache(digit, mul(left, right));
+        addCache(digit, div(left, right));
+    }
+
+    public static int solution(int N, int number) {
+
+        if (N == number) {
+            return 1;
+        }
+
+        int temp = N;
+        for (int i = 1; i < 9; i++) {
+            if (temp == number)
+                return i;
+            cache[i] = new ArrayList<Integer>();
+            cache[i].add(temp);
+            check.add(temp);
+            temp *= 10;
+            temp += N;
+        }
+
+        for (int digit = 1; digit < 9; digit++) { // 3
+            for (int i = 1; i < digit; i++) { // 1, 2
+                int j = digit - i; // i,j = 1,2 // 2,1
+                for (int left : cache[i]) {
+                    for (int right : cache[j]) {
+                        cal(digit, left, right);
+                        if (check.contains(number)) {
+                            return digit;
+                        }
+                    }
                 }
             }
         }
-
+        return -1;
     }
 
     public static void main(String[] args) {
@@ -66,4 +78,5 @@ public class DP_1_1 {
         int number = 12;
         System.out.println(solution(N, number));
     }
+
 }
