6ff3d6019b5f7bc00262967739eb5cf2d5ff1592
Blackbutterfly0852 <blackbutterfly0852@gmail.com>
2020-10-21 22:21:19 +0900

    201021 Heap

--- 3. DSAL_REVIEW/1. StackQueue/R_StackQueue_1.java
+++ 3. DSAL_REVIEW/1. StackQueue/R_StackQueue_1.java
@@ -4,20 +4,7 @@ public class R_StackQueue_1 {
 
     public static int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        for (int i = 0; i < prices.length; i++) {
-            for (int j = i + 1; j < prices.length; j++) {
-                // 참고
-                // 주어준 배열의 원소당 조건에 맞는 개수를 구하는 경우
-                // 주어진 배열길이로 새로운 배열을 생성 후, 새로운 배열에서 count 할 수 있다.
-                answer[i]++;
-                if (prices[i] > prices[j]) {
-                    break;
-                }
-            }
-        }
-
-        System.out.println(Arrays.toString(answer));
-        // Arrays.toString(answer);
+       
         return answer;
     }
 
--- 3. DSAL_REVIEW/1. StackQueue/R_StackQueue_2.java
+++ 3. DSAL_REVIEW/1. StackQueue/R_StackQueue_2.java
@@ -1,3 +1,19 @@
+import java.util.*;
+
 public class R_StackQueue_2 {
-    
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+      
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1,30,5};
+        
+        solution(progresses, speeds);
+    }
 }
