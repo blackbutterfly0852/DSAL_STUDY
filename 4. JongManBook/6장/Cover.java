c5ac9c1c94188758a90a5f41202f2e7c4e796ded
Blackbutterfly0852 <blackbutterfly0852@gmail.com>
2020-11-08 17:41:22 +0900

    201108 Cover

--- 4. JongManBook/6장/Cover.java
+++ 4. JongManBook/6장/Cover.java
@@ -1,74 +1,104 @@
+import java.util.*;
+
 public class Cover {
-    // 덮힘 : true;
-    // 안덮힘 : false;
+    static int[][][] coverType = { { { 0, 0 }, { 0, 1 }, { 1, 0 } }, { { 0, 0 }, { 0, 1 }, { 1, 1 } },
+            { { 0, 0 }, { 1, 0 }, { 1, 1 } }, { { 0, 0 }, { 1, 0 }, { 1, -1 } } };
 
-    static int[][][] coverType = {
+    static int[][] board;
 
-        {{0,0} , {0,1} , {1,0}},
-        {{0,0} , {0,1} , {1,1}},
-        {{0,0} , {1,0} , {1,1}},
-        {{0,0} , {1,0} , {1,-1}}
-    };
+    // 1. init
+    // 1 : 덮힘, 0 : 안덮힘
+    public static void init(int x, int y, String[][] current) {
+        board = new int[x][y];
 
-
-    
-
-    static boolean[][] isVisited;
-
-    public static boolean[][] init(int x, int y, String[][] current) {
-        isVisited = new boolean[x][y];
         for (int i = 0; i < current.length; i++) {
-            for (int j = 0; j < current[i].length; j++) {
-                if (current[i][j].equals("#")) {
-                    isVisited[i][j] = true;
+            String[] c = current[i][0].split("");
+            for (int j = 0; j < c.length; j++) {
+                if (c[j].equals("#")) {
+                    board[i][j] = 1;
                 }
             }
         }
-        return isVisited;
+
+        System.out.println(cover(board));
     }
 
-    // 덮힘 : true;
-    // 안덮힘 : false;
-    public static boolean isPossible(boolean[][] isVisited) {
-        int count = 0;
-        for (int i = 0; i < isVisited.length; i++) {
-            for (int j = 0; j < isVisited[i].length; j++) {
-                if (!isVisited[i][j]) {
-                    count++;
+    // 2.
+
+    public static int cover(int[][] board) {
+
+        // 1. 기저사례
+        // 1) 안덮힌 개수 % 3 !=0 인경우
+        int unCoverCnt = 0;
+        for (int[] bd : board) {
+            for (int b : bd) {
+                if (b == 0) {
+                    unCoverCnt++;
                 }
             }
         }
-        return count % 3 == 0 ? true : false;
-    }
 
-    public static int converCount(boolean[][] isVisited, int[][] shape) {
-
-        // 기저 사례 1 : 안덮힘 개수가 % 3 !=0이 아닌 경우
-        if (!isPossible(isVisited)) {
+        if (unCoverCnt % 3 != 0) {
+            System.out.println("기저사례 1");
             return 0;
         }
 
-        // 기저 사례 2 : isAllCover true 경우 모두 다 덮힌 경우이다.
-        boolean isAllCover = true;
-        // 덮힘 : true;
-        // 안덮힘 : false;
-        for (int i = 0; i < isVisited.length; i++) {
-            for (int j = 0; j < isVisited[i].length; j++) {
-                if (!isVisited[i][j]) {
-                    isAllCover = false;
-                    break;
+        // 2) 모두 다 덮은 경우
+        int x = -1;
+        int y = -1;
+        loop: for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 0) {
+                    x = i;
+                    y = j;
+                    break loop;
                 }
             }
         }
-        if (isAllCover) {
+
+        if (x == -1 && y == -1) {
             return 1;
         }
 
         int ret = 0;
-        for(int i = 0 ; i < )
-        
+        for (int type = 0; type < 4; type++) {
+            // 유효성 통과 여부
+            if (isGoing(x, y, type, 1)) {
+                ret += cover(board);
+            }
+            // 유효성 통과 여부와는 상관없이 이전에 덮은 걸 원상복구 시킨다.
+            isGoing(x, y, type, -1);
+        }
 
         return ret;
+
+    }
+
+    // 3. 유효성 검사
+    public static boolean isGoing(int x, int y, int type, int flag) {
+        boolean answer = true;
+
+        for (int i = 0; i < 3; i++) {
+
+            int moveX = x + coverType[type][i][0];
+            int moveY = y + coverType[type][i][1];
+
+            // 1) 범위가 넘은 경우
+            if (moveX < 0 || moveX >= board.length || moveY < 0 || moveY >= board[moveX].length) {
+                answer = false;
+                // 2) 이미 덮여 있는 경우
+                // * 우선 1을 더해준다
+                // * 나중에 원상복구 시킬 때 안덮힌경우로 변경되는걸 방지위해
+            } else if (board[moveX][moveY] + flag > 1) {
+                board[moveX][moveY] += flag;
+                answer = false;
+                // 3) 그 외엔 정상이므로 덮어 준다.
+            } else {
+                board[moveX][moveY] += flag;
+            }
+
+        }
+        return answer;
 
     }
 
@@ -76,8 +106,13 @@ public class Cover {
         int x = 3;
         int y = 7;
         String[][] current = { { "#.....#" }, { "#.....#" }, { "##..###" } };
-        init(x, y, current);
 
+        // int x = 8;
+        // int y = 10;
+        // String[][] current = { { "##########" }, { "#........#" }, { "#........#" },
+        // { "#........#" }, { "#........#" },{ "#........#" }, { "#........#" }, {
+        // "##########" } };
+        init(x, y, current);
     }
 
 }
