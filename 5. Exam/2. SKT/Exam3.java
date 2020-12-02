d8feda7beb0898a3efa9362ba76febc906a6580c
Blackbutterfly0852 <blackbutterfly0852@gmail.com>
2020-12-02 20:44:02 +0900

    201202 SKT_3

--- 5. Exam/2. SKT/Exam3.java
+++ 5. Exam/2. SKT/Exam3.java
@@ -1,51 +1,79 @@
 public class Exam3 {
-    static int bigCnt = 0;
-    static int smallCnt = 0;
-    static String ret = "";
-    static int totalCnt = 0;
-
     public static String solution(int A, int B) {
-        totalCnt = A + B;
-        if (A > B) {
-            return solution_2(A, B, "a", "b", "");
+        
+        if (A >= B) {
+            return findString(A, B, "a", "b" );
         } else {
-            return solution_2(B, A, "b", "a", "");
+            return findString(B, A, "b", "a" );
         }
     };
 
-    public static String solution_2(int bigInt, int smallInt, String big, String small, String flag) {
-        if (ret.length() > totalCnt) {
-            return "";
-        }
+    public static String findString(int bigInt, int smallInt, String bigS, String smallS) {
+        int curBigInt = bigInt;
+        int curSmallInt = smallInt;
+        int bigCnt = 0;
+        int smallCnt = 0;
+        String answer = "";
 
-        // if(flag.equals(big)) return big;
-        // if(flag.equals(small)) return small;
-
-        while (bigInt > 0 || smallInt > 0) {
-            
-            if (bigInt > 0 && bigCnt < 2) {
-                bigCnt += 1;
-                bigInt--;
+        while (curBigInt != 0 || curSmallInt != 0) {
+            System.out.println("answer = " + answer);
+            System.out.println("answer.length = " + answer.length());
+            while (bigCnt < 2 && curBigInt > 0) {
+                answer += bigS;
+                bigCnt++;
+                curBigInt--;
                 smallCnt = 0;
-                ret += big;
             }
-            if (smallInt > 0 && smallCnt < 2) {
-                smallCnt += 1;
-                smallInt--;
+
+            while (smallCnt < 2 && curSmallInt > 0) {
+                answer += smallS;
+                smallCnt++;
+                curSmallInt--;
                 bigCnt = 0;
-                ret += small;
             }
 
         }
 
-        return ret;
-
+        return answer;
     }
 
+    // 탐욕 알고리즘
+    public static String solution_2(int A, int B) {
+        StringBuilder sb = new StringBuilder();
+        
+        while(A>0 || B>0){
+            boolean writeA = false;
+            int size = sb.length();
+            // 1. A를 작성할 지 B를 작성할 지 결정
+            if(sb.length() >=2 && sb.charAt(size-1) == sb.charAt(size-2)){
+                if(sb.charAt(size-1) == 'b'){
+                    writeA = true;
+                }
+            }else if( A >= B){
+                    writeA = true;
+            }
+
+            // 2. 위에서 결정된 내용을 가지고 StringBuilder에 추가
+            if(writeA){
+                sb.append('a');
+                A--;
+            }else{
+                sb.append('b');
+                B--;
+            }
+
+        }
+
+
+
+
+        return sb.toString();
+
+    };
     public static void main(String[] args) {
         int A = 4;
         int B = 1;
-        System.out.println(solution(A, B));
+        System.out.println(solution_2(2, 5));
 
     }
 
